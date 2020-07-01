93bdeed7ac4c2e75386824616ce52b7a195e511a
zoooooo <n_shimazu@innovation.co.jp>
2020-07-01 16:23:06 +0900

    リファクタリング

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -22,93 +22,14 @@ class TableController extends Controller
 
     public function add(InsertRequest $request)
     {
-        //段位を数値化
-        $grade_number = [
-            '新人' => ['number' => 0, 'start' => 0, 'upgrade' => 20],
-            '９級' => ['number' => 1, 'start' => 0, 'upgrade' => 20],
-            '８級' => ['number' => 2, 'start' => 0, 'upgrade' => 20],
-            '７級' => ['number' => 3, 'start' => 0, 'upgrade' => 20],
-            '６級' => ['number' => 4, 'start' => 0, 'upgrade' => 40],
-            '５級' => ['number' => 5, 'start' => 0, 'upgrade' => 60],
-            '４級' => ['number' => 6, 'start' => 0, 'upgrade' => 80],
-            '３級' => ['number' => 7, 'start' => 0, 'upgrade' => 100],
-            '２級' => ['number' => 8, 'start' => 0, 'upgrade' => 100],
-            '１級' => ['number' => 9, 'start' => 0, 'upgrade' => 100],
-            '初段' => ['number' => 10, 'start' => 200, 'upgrade' => 400],
-            '二段' => ['number' => 11, 'start' => 400, 'upgrade' => 800],
-            '三段' => ['number' => 12, 'start' => 600, 'upgrade' => 1200],
-            '四段' => ['number' => 13, 'start' => 800, 'upgrade' => 1600],
-            '五段' => ['number' => 14, 'start' => 1000, 'upgrade' => 2000],
-            '六段' => ['number' => 15, 'start' => 1200, 'upgrade' => 2400],
-            '七段' => ['number' => 16, 'start' => 1400, 'upgrade' => 2800],
-            '八段' => ['number' => 17, 'start' => 1600, 'upgrade' => 3200],
-            '九段' => ['number' => 18, 'start' => 1800, 'upgrade' => 3600],
-            '十段' => ['number' => 19, 'start' => 2000, 'upgrade' => 4000],
-            '天鳳位' => ['number' => 20],
-        ];
         $data = [];
-        $file = new \SplFileObject($request->csvfile);
-      
-        $file->setFlags(
-            \SplFileObject::READ_CSV |           // CSV 列として行を読み込む
-            \SplFileObject::READ_AHEAD |       // 先読み/巻き戻しで読み出す。
-            \SplFileObject::SKIP_EMPTY |         // 空行は読み飛ばす
-            \SplFileObject::DROP_NEW_LINE    // 行末の改行を読み飛ばす
-        );
-        //読み込んだcsvファイルを1行ずつ配列に入れる
-        foreach ($file as $line) {
-            $pre_data[] = $line;
-        }
 
-        $grade_array_key = array_keys($pre_data[0], '段位')[0];
-        $point_array_key = array_keys($pre_data[0], 'pt')[0];
-        $fluctuation_array_key = array_keys($pre_data[0], 'ptの変動')[0];
-        if (!empty(array_keys($pre_data[0], '個室'))) {
-            $private_array_key = array_keys($pre_data[0], '個室')[0];
-        } else {
-            $private_array_key = 100;
-        }
-        $rule_array_key = array_keys($pre_data[0], 'ルール')[0];
+        $file = $this->table_service->readPlayerData($request->csvfile);
+        $data_titles = $this->table_service->getDataTitles($file);
+        $data_keys = $this->table_service->getArrayKeys($data_titles);
+        $data = $this->table_service->setPlayerData($file, $data_keys);
 
-        foreach ($pre_data as $pre_datum) {
-            if (empty($pre_datum[$private_array_key]) && strpos($pre_datum[$rule_array_key], '三') === false) {
-                $data[] = [
-                    'grade' => $pre_datum[$grade_array_key],
-                    'point' => intval($pre_datum[$point_array_key]),
-                    'fluctuation' => intval($pre_datum[$fluctuation_array_key]),
-                ];
-            }
-        }
-        $frequency = count($data);//打数
-        $last_month_data = $data[1];//月初のデータ
-        $latest_data = end($data);//月終わりのデータ
-        $tenhou = app(Tenhou::class);
-        $tenhou->real_name = $request->real_name;
-        $tenhou->tenhou_name = $request->tenhou_name;
-        $tenhou->twitter_id = $request->twitter_id;
-        $tenhou->month = $request->month;
-        $tenhou->last_month_grade = $last_month_data['grade'];
-        $tenhou->last_month_point = $last_month_data['point'];
-        $latest_grade_number = $grade_number[$latest_data['grade']]['number'];
-        $latest_point = $latest_data['point'] + $latest_data['fluctuation'];
-        if ($latest_point < 0) {
-            $latest_grade_number -= 1;
-            $latest_point = $grade_number[$latest_data['grade']]['start'];
-        } else if($latest_point >= $grade_number[$latest_data['grade']]['upgrade']) {
-            $latest_grade_number += 1;
-            $latest_point = $grade_number[$latest_data['grade']]['start'];
-        }
-        $tenhou->latest_grade = array_keys(array_filter($grade_number, function($grade_number) use($latest_grade_number){
-            return $grade_number['number'] == $latest_grade_number;
-        }))[0];
-        if ($latest_grade_number > $grade_number[$last_month_data['grade']]['number']) {
-            $tenhou->upgrade = 1;
-        } else if ($latest_grade_number < $grade_number[$last_month_data['grade']]['number']) {
-            $tenhou->downgrade = 1;
-        }
-        $tenhou->latest_point = $latest_point;
-        $tenhou->frequency = $frequency;
-        $tenhou->save();
+        $this->table_service->savePlayerData($data, $request);
 
         return redirect('/tenhou');
     }
@@ -124,32 +45,9 @@ class TableController extends Controller
     {
 
         $members = Tenhou::all()->sortByDesc(function($member) {
-            $grade_number = [
-                '新人' => ['number' => 0, 'start' => 0, 'upgrade' => 20],
-                '９級' => ['number' => 1, 'start' => 0, 'upgrade' => 20],
-                '８級' => ['number' => 2, 'start' => 0, 'upgrade' => 20],
-                '７級' => ['number' => 3, 'start' => 0, 'upgrade' => 20],
-                '６級' => ['number' => 4, 'start' => 0, 'upgrade' => 40],
-                '５級' => ['number' => 5, 'start' => 0, 'upgrade' => 60],
-                '４級' => ['number' => 6, 'start' => 0, 'upgrade' => 80],
-                '３級' => ['number' => 7, 'start' => 0, 'upgrade' => 100],
-                '２級' => ['number' => 8, 'start' => 0, 'upgrade' => 100],
-                '１級' => ['number' => 9, 'start' => 0, 'upgrade' => 100],
-                '初段' => ['number' => 10, 'start' => 200, 'upgrade' => 400],
-                '二段' => ['number' => 11, 'start' => 400, 'upgrade' => 800],
-                '三段' => ['number' => 12, 'start' => 600, 'upgrade' => 1200],
-                '四段' => ['number' => 13, 'start' => 800, 'upgrade' => 1600],
-                '五段' => ['number' => 14, 'start' => 1000, 'upgrade' => 2000],
-                '六段' => ['number' => 15, 'start' => 1200, 'upgrade' => 2400],
-                '七段' => ['number' => 16, 'start' => 1400, 'upgrade' => 2800],
-                '八段' => ['number' => 17, 'start' => 1600, 'upgrade' => 3200],
-                '九段' => ['number' => 18, 'start' => 1800, 'upgrade' => 3600],
-                '十段' => ['number' => 19, 'start' => 2000, 'upgrade' => 4000],
-                '天鳳位' => ['number' => 20],
-            ];
-            
-            return $grade_number[$member->latest_grade]['number'];
+            return config('const.GRADE_NUMBER')[$member->latest_grade]['number'];
         });
+
         return view('screenshot', compact('members'));
     }
 }
--- app/Services/TableService.php
+++ app/Services/TableService.php
@@ -1,7 +1,153 @@
 <?php
 namespace App\Services;
 
+use App\Tenhou;
+
 class TableService
 {
-    
+    public function readPlayerData($file)
+    {
+        $file = new \SplFileObject($file);
+      
+        $file->setFlags(
+            \SplFileObject::READ_CSV |           // CSV 列として行を読み込む
+            \SplFileObject::READ_AHEAD |       // 先読み/巻き戻しで読み出す。
+            \SplFileObject::SKIP_EMPTY |         // 空行は読み飛ばす
+            \SplFileObject::DROP_NEW_LINE    // 行末の改行を読み飛ばす
+        );
+        
+        return $file;
+    }
+
+    public function getDataTitles($file)
+    {
+        foreach ($file as $line) {
+            $data_titles[] = $line;
+        }
+
+        return $data_titles[0];
+    }
+
+    public function getArrayKeys($data_titles)
+    {
+        $grade_key = array_keys($data_titles, '段位')[0];
+        $point_key = array_keys($data_titles, 'pt')[0];
+        $fluctuation_key = array_keys($data_titles, 'ptの変動')[0];
+        if (!empty(array_keys($data_titles, '個室'))) {
+            $private_key = array_keys($data_titles, '個室')[0];
+        } else {
+            $private_key = 100;
+        }
+        $rule_key = array_keys($data_titles, 'ルール')[0];
+
+        return compact(
+            'grade_key', 
+            'point_key', 
+            'fluctuation_key', 
+            'private_key',
+            'rule_key'
+        );
+    }
+
+    public function setPlayerData($file, $data_keys)
+    {
+        foreach ($file as $line) {
+            if (empty($line[$data_keys['private_key']]) && $this->isThreePlayer($line[$data_keys['rule_key']]) === false) {
+                $data[] = [
+                    'grade' => $line[$data_keys['grade_key']],
+                    'point' => intval($line[$data_keys['point_key']]),
+                    'fluctuation' => intval($line[$data_keys['fluctuation_key']]),
+                ];
+            }
+        }
+        return $data;
+    }
+
+    public function isThreePlayer($rule_key)
+    {
+        return strpos($rule_key, '三');
+    }
+
+    public function savePlayerData($data, $request)
+    {
+        $frequency = count($data);//打数
+        $last_month_data = $data[0];//月初のデータ
+        $latest_data = end($data);//月終わりのデータ
+
+        $tenhou = app(Tenhou::class);
+
+        $tenhou->real_name = $request->real_name;
+        $tenhou->tenhou_name = $request->tenhou_name;
+        $tenhou->twitter_id = $request->twitter_id;
+        $tenhou->month = $request->month;
+
+        $tenhou->last_month_grade = $last_month_data['grade'];
+        $tenhou->last_month_point = $last_month_data['point'];
+
+        $latest_grade_number = $this->checkLastGrade($latest_data);
+        $latest_point = $this->checkLastPoint($latest_data);
+        $latest_point = $latest_data['point'] + $latest_data['fluctuation'];
+
+        $tenhou->latest_grade = $this->convertIntoGrade($latest_grade_number);
+        
+        $tenhou->upgrade = $this->checkUpgrade($latest_grade_number, $last_month_data);
+        $tenhou->downgrade = $this->checkDowngrade($latest_grade_number, $last_month_data);
+
+        $tenhou->latest_point = $latest_point;
+        $tenhou->frequency = $frequency;
+        $tenhou->save();
+
+    }
+
+    public function checkLastGrade($latest_data)
+    {
+        $latest_grade_number = config('const.GRADE_NUMBER')[$latest_data['grade']]['number'];
+        $latest_point = $latest_data['point'] + $latest_data['fluctuation'];
+        if ($latest_point < 0) {
+            $latest_grade_number -= 1;
+        } else if($latest_point >= config('const.GRADE_NUMBER')[$latest_data['grade']]['upgrade']) {
+            $latest_grade_number += 1;
+        }
+
+        return $latest_grade_number;
+    }
+
+    public function checkLastPoint($latest_data)
+    {
+        $latest_point = $latest_data['point'] + $latest_data['fluctuation'];
+
+        if ($latest_point < 0) {
+            $latest_point = config('const.GRADE_NUMBER')[$latest_data['grade']]['start'];
+        } else if($latest_point >= config('const.GRADE_NUMBER')[$latest_data['grade']]['upgrade']) {
+            $latest_point = config('const.GRADE_NUMBER')[$latest_data['grade']]['start'];
+        }
+
+        return $latest_point;
+    }
+
+    public function convertIntoGrade($latest_grade_number)
+    {
+        $grade_array = config('const.GRADE_NUMBER');
+        return array_keys(array_filter($grade_array, function($grade_array) use($latest_grade_number){
+                    return $grade_array['number'] == $latest_grade_number;
+               }))[0];
+    }
+
+    public function checkUpgrade($latest_grade_number, $last_month_data)
+    {
+        if ($latest_grade_number > config('const.GRADE_NUMBER')[$last_month_data['grade']]['number']) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public function checkDowngrade($latest_grade_number, $last_month_data)
+    {
+        if ($latest_grade_number < config('const.GRADE_NUMBER')[$last_month_data['grade']]['number']) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
